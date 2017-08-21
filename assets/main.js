b8647f0b07b44a2a82528f5c53388c224ec6d9b6
Javier Bertos <javier.bertos@financefox.com>
2017-08-21 03:52:46 +0200

    #7 Set the hidden input attempt's value to zero

--- assets/main.js
+++ assets/main.js
@@ -9,4 +9,5 @@ function guess() {
 //implement new functions here
 function setHiddenFields() {
     answer.value = ("0000" + Math.floor(Math.random() * 9999).toString()).slice(-4);
+    attempt.value = 0;
 }
